ad618f1f2fd80db15869d94be00a9d3f7696c806
coaxialinstaller <119482085+coaxialinstaller@users.noreply.github.com>
2023-03-07 08:00:15 +0100

    Fix

--- install.sh
+++ install.sh
@@ -6,28 +6,99 @@ then
     exit 1
 fi
 
+repeatChar() {
+    local input="$1"
+    local count="$2"
+    printf -v myString '%*s' "$count"
+    printf '%s\n' "${myString// /$input}"
+}
+
+
 sudo echo &>/dev/null
 
 program=$1
 
 which apt 2>/dev/null | grep /apt &>/dev/null && PM="apt"
 which pacman 2>/dev/null | grep /pacman &>/dev/null && PM="pacman"
-[[ $PM == "" ]] && echo No && exit 0
+[[ $PM == "" ]] && echo "Error: Couldn't recognize package manager..." && exit 0
+
+
+fail=false
 
 exit=false
 ls $program-install &>/dev/null || exit=true
-$exit && echo "Program not install in the directory"
+$exit && echo "Error: Couldn't find packages to install..."
 $exit && exit 0
+
+
 
 if [[ $PM == "pacman" ]]
 then
+
+    packages=$(ls $program-install | grep -v .sig | wc -l)
+    po=30
+    fo=0
+
     echo "Installing $program..."
+    big=$(((($po*100/$packages)*$fo)/100))
+    small=$(($po-$big))
+    str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+    echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+
     for i in $(ls $program-install | grep -v .sig)
     do
         sudo pacman -U --noconfirm $program-install/$i &>/dev/null
+
+        fo=$(($fo+1))
+
+        big=$(((($po*100/$packages)*$fo)/100))
+        small=$(($po-$big))
+        str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+        echo -ne "\r[$str] ($(($fo*100/$packages))%)"
     done
+
+    $fail && echo && echo "Error downloading $program..."
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished Downloading $program."
+
 elif [[ $PM == "apt" ]]
 then
+
+    packages=$(cat dependencies | wc -l)
+    po=30
+    fo=0
+
     echo "Installing $program..."
-    sudo dpkg -i $program-install/* &>/dev/null
+    big=$(((($po*100/$packages)*$fo)/100))
+    small=$(($po-$big))
+    str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+    echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+    for i in $(ls $program-install | grep -v .sig)
+    do
+
+        sudo dpkg -i $program-install/* &>/dev/null || fail=true
+        $fail && break
+
+        fo=$(($fo+1))
+
+        big=$(((($po*100/$packages)*$fo)/100))
+        small=$(($po-$big))
+        str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+        echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+    done
+
+    $fail && echo && echo "Error downloading $program..."
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished Downloading $program."
 fi
